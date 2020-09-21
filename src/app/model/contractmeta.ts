9e14e20f1c4df890f9f0f58e4672a21690dcf738
Manish S <smanishh9@gmail.com>
2020-09-21 14:27:34 +0530

    models

---
+++ src/app/model/contractmeta.ts
@@ -0,0 +1,17 @@
+export class ContractMeta {
+ headerName: string;
+ headerPageNo: number;
+ headerTextContent: string;
+ headerParagraphIndex: number;
+ headerFontName: string;
+ headerFontSize: string;
+ startLocationX: number;
+ startLocationY: number;
+ endLocationX: number;
+ endLocationY: number;
+
+//
+// textSimilarity: number;
+// domaincontextCurrentFieldValue: string;
+// domaincontextProposedFieldValue: string;
+}
---
+++ src/app/model/global.ts
@@ -0,0 +1,13 @@
+export class Global {
+    regulatoryEventId: number;
+    contractType: number;
+    domainContextDictionaryId: string;
+    domainContextName: string;
+    domainContextTypeId: number;
+    domainContextSubTypeId: number;
+    domainContextPossibleNameDefinitions: string;
+    domainContextPossibleValueDefinitions: string;
+    phraseRule: string;
+    entityRule: string;
+    referenceExamples: string;
+}
